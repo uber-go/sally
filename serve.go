76c64ffa5e904c0a4bcb4f950a82501517419d8f
Grayson Koonce <grayson@uber.com>
2016-10-01 14:59:47 -0700

    only route packages from sally.yaml

--- serve.go
+++ serve.go
@@ -13,8 +13,12 @@ func Serve(config Config) error {
 	router.RedirectTrailingSlash = false
 
 	router.GET("/", handleIndex)
-	router.GET("/package", handlePackage)
-	router.GET("/package/*name", handlePackage)
+
+	for name, pkg := range config.Packages {
+		fmt.Println(pkg)
+		router.GET(fmt.Sprintf("/%s", name), handlePackage)
+		router.GET(fmt.Sprintf("/%s/*name", name), handlePackage)
+	}
 
 	// TODO port should be cli opt
 	if err := http.ListenAndServe(":8080", router); err != nil {
