9de57d61b33c50890d9f686ad27047d6bb43686a
Grayson Koonce <grayson@uber.com>
2016-10-01 14:54:03 -0700

    get url patterns right using httprouter

--- main.go
+++ main.go
@@ -5,17 +5,6 @@ import (
 	"log"
 )
 
-// Config represents the structure of the yaml file
-type Config struct {
-	URL      string             `yaml:"url"`
-	Packages map[string]Package `yaml:"packages"`
-}
-
-// Package details the options available for each repo
-type Package struct {
-	Repo string `yaml:"repo"`
-}
-
 func main() {
 	yml := flag.String("yml", "sally.yaml", "yaml file to read config from")
 	flag.Parse()
--- serve.go
+++ serve.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // Serve starts the HTTP server
 func Serve(config Config) error {
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := httprouter.New()
+	router.RedirectTrailingSlash = false
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	router.GET("/", handleIndex)
+	router.GET("/package", handlePackage)
+	router.GET("/package/*name", handlePackage)
+
+	// TODO port should be cli opt
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func handlePackage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
