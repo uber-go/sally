d259db8d14c83733af3adfadf272b1e8f3db83a4
Grayson Koonce <grayson@uber.com>
2016-10-02 10:14:27 -0700

    test Parse

--- config_test.go
+++ config_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	yml := `
+url: google.golang.org
+packages:
+  grpc:
+    repo: github.com/grpc/grpc-go
+`
+	path, clean := TempFile(t, yml)
+	defer clean()
+
+	config, err := Parse(path)
+	assert.NoError(t, err)
+
+	assert.Equal(t, config.URL, "google.golang.org")
+
+	pkg, ok := config.Packages["grpc"]
+	assert.True(t, ok)
+
+	assert.Equal(t, pkg, Package{Repo: "github.com/grpc/grpc-go"})
+}
