210322b0dc2d83137ee3b97951e22a1bfc96cd8a
Grayson Koonce <grayson@uber.com>
2016-10-03 14:26:35 -0700

    move html near code that uses it

--- handler.go
+++ handler.go
@@ -8,33 +8,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var indexTemplate = template.Must(template.New("index").Parse(`
-<!DOCTYPE html>
-<html>
-    <body>
-        <ul>
-            {{ range $key, $value := .Packages }}
-	  	        <li>{{ $key }} - {{ $value.Repo }}</li>
-	        {{ end }}
-        </ul>
-    </body>
-</html>
-`))
-
-var packageTemplate = template.Must(template.New("package").Parse(`
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="go-import" content="{{ .CanonicalURL }} git https://{{ .Repo }}">
-        <meta name="go-source" content="{{ .CanonicalURL }} https://{{ .Repo }} https://{{ .Repo }}/tree/master{/dir} https://{{ .Repo }}/tree/master{/dir}/{file}#L{line}">
-        <meta http-equiv="refresh" content="0; url={{ .GodocURL }}">
-    </head>
-    <body>
-        Nothing to see here. Please <a href="{{ .GodocURL }}">move along</a>.
-    </body>
-</html>
-`))
-
 // CreateHandler creates a Sally http.Handler
 func CreateHandler(config Config) http.Handler {
 	router := httprouter.New()
@@ -63,6 +36,19 @@ func (h indexHandler) Handle(w http.ResponseWriter, r *http.Request, _ httproute
 	indexTemplate.Execute(w, h.config)
 }
 
+var indexTemplate = template.Must(template.New("index").Parse(`
+<!DOCTYPE html>
+<html>
+    <body>
+        <ul>
+            {{ range $key, $value := .Packages }}
+	  	        <li>{{ $key }} - {{ $value.Repo }}</li>
+	        {{ end }}
+        </ul>
+    </body>
+</html>
+`))
+
 type packageHandler struct {
 	pkgName string
 	pkg     Package
@@ -81,3 +67,17 @@ func (h packageHandler) Handle(w http.ResponseWriter, r *http.Request, ps httpro
 		GodocURL:     fmt.Sprintf("https://godoc.org/%s%s", canonicalURL, ps.ByName("path")),
 	})
 }
+
+var packageTemplate = template.Must(template.New("package").Parse(`
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="go-import" content="{{ .CanonicalURL }} git https://{{ .Repo }}">
+        <meta name="go-source" content="{{ .CanonicalURL }} https://{{ .Repo }} https://{{ .Repo }}/tree/master{/dir} https://{{ .Repo }}/tree/master{/dir}/{file}#L{line}">
+        <meta http-equiv="refresh" content="0; url={{ .GodocURL }}">
+    </head>
+    <body>
+        Nothing to see here. Please <a href="{{ .GodocURL }}">move along</a>.
+    </body>
+</html>
+`))
