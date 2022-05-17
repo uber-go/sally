1862d6f40e1a8cdc463bb1adf3f400d2dd85f475
Abhinav Gupta <abg@uber.com>
2022-05-17 09:31:14 -0700

    Update dependencies
    Update all dependencies,
    use Go 1.18 to build and test,
    and delete outdated `bindata` clause in Makefile
    (we switched to `go:embed` in #50).
    
    Other minor changes:
    
    - Fix the golint import path
      (it switched to golang.org/x/lint long before it was deprecated)
      and fix the failing lint check.
      (Switching from golint to revive is out of scope for this PR.)
    - Delete .envrc -- this was transitional for when Go modules were
      optional.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.17
+FROM golang:1.18
 
 EXPOSE 8080
 RUN \
--- templates/templates.go
+++ templates/templates.go
@@ -1,9 +1,11 @@
 package templates
 
-import _ "embed"
+import _ "embed" // needed for go:embed
 
+// Index holds the contents of the index.html template.
 //go:embed index.html
 var Index string
 
+// Package holds the contents of the package.html template.
 //go:embed package.html
 var Package string
